f1f6f062c3d363a9cf725e38cb2a5706d9061cb5
rafalWili <rafal.wili.uk@gmail.com>
2021-04-24 23:05:09 +0200

    pie chart basics ready

--- src/js/components/DetailsComponent.js
+++ src/js/components/DetailsComponent.js
@@ -17,7 +17,7 @@ const options = {
 
 
 let styles = {
-    card : { width: '100%', minHeight: '60vh', padding: '20px', borderColor: '#000', background: 'lightblue' }
+    card : { width: '100%', minHeight: '60vh', padding: '20px', borderColor: '#000', background: 'white' }
 }
 
 class DetailsComponent extends Component {
@@ -30,12 +30,20 @@ class DetailsComponent extends Component {
     }
 
     render(){
-let options = [];
+let options  = [];
+let countries = [];
 let performance = this.props.item.value.Performance;
+let AllocationCountry = this.props.item.value.AllocationCountry;
 
 performance.map( (value, index) => {
     options.push([ Date.UTC(value.Date.split('-')[0], 11, 1), value.Value] )
-})
+});
+AllocationCountry.map( (value, index) => {
+    console.log(value.Key)
+    console.log(value.Value)
+    countries.push( {name: value.Key, y: value.Value } )
+});
+console.log(countries)
 
         let details = this.props.item;
         return(
@@ -48,6 +56,10 @@ performance.map( (value, index) => {
                     options={{  xAxis: {
                                     type: 'datetime'
                                 },
+                                yAxis: {
+                                    min : 80,
+                                    max: 150
+                                },
                                 title: {
                                 text: 'My chart'
                                 },
@@ -57,8 +69,46 @@ performance.map( (value, index) => {
                                 }]
                             }}
                                             />
-                
-                  
+                         <h4 style={{ margin: '20px 0' }}>Country Allocation</h4>
+                       <HighchartsReact
+                    highcharts={Highcharts}
+                    options={{ 
+                        chart: {
+                                    plotBackgroundColor: null,
+                                    plotBorderWidth: null,
+                                    plotShadow: false,
+                                    type: 'pie'
+                                },
+                                title: {
+                                    text: 'Browser market shares in January, 2018'
+                                },
+                                tooltip: {
+                                    pointFormat: '{series.name}: <b>{point.percentage:.0f}%</b>'
+                                },
+                                accessibility: {
+                                    point: {
+                                        valueSuffix: '%'
+                                    }
+                                },
+                                plotOptions: {
+                                    pie: {
+                                        allowPointSelect: true,
+                                        cursor: 'pointer',
+                                        dataLabels: {
+                                            enabled: false,
+                                            format: '<b>{point.name}</b>: {point.percentage:.0f} %'
+                                        }
+                                    }
+                                },
+                                series: [{
+                                    name: 'Brands',
+                                    colorByPoint: true,
+                                    data: countries
+                                }]
+                            }}
+                                            />
+
+        
             </Card>
 
         )
